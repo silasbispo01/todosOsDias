4f20d5b868ca7f112b78f9466e7e5f45d1820e3c
silasbispo01 <silasbispo01@gmail.com>
2021-05-05 19:04:14 -0300

    Create arrays.js

---
+++ Todos os dias/dia 9/arrays.js
@@ -0,0 +1,27 @@
+const alunos = ['João','Maria','Claudia'];
+// alunos[3] = 'Luiz' // altera ou adiciona
+// alunos[alunos.length] = 'Lucas' // @@ adiciona ao fim!
+// alunos.push('Luiz'); // @@@@@@@@@@@@@ adiciona ao fim!
+// alunos.pop(); // @@@@@@@@@@@@@@@@@@@@ remove ao fim
+// alunos.unshift('Lagertha'); // @@@@@@ adiciona ao inicio
+// alunos.unshift('Ragnar'); // @@@@@@@@ adiciona ao inicio 
+// alunos.shift(); // @@@@@@@@@@@@@@@@@@ remove ao inicio
+// const removidos = alunos.shift();
+// console.log(removidos);
+// console.log(alunos);
+// console.log(alunos[2])
+
+
+
+
+n = (11)
+function parimp(n) {
+    if(n%2==0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+let res = parimp(n)
+console.log(res);
