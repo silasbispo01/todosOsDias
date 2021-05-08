019b9dd5dd202cc4be2ce90aa0e09fe432427234
silasbispo01 <silasbispo01@gmail.com>
2021-05-08 20:10:57 -0300

    todosOsDias

--- Todos os dias/dia 10/exerc/js.js
+++ Todos os dias/dia 10/exerc/js.js
@@ -1,5 +1,34 @@
-function container (){
+function escopo () {
+    const form = document.querySelector('.formulario');
+    const resultado = document.querySelector('.resultado')
+    const pessoas = [];
 
-}  
+    function recebeEventoForm (evento) {
+        evento.preventDefault(); 
 
-container();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+        console.log(pessoas)
+
+        resultado.innerHTML += (`<p> ${nome.value} ${sobrenome.value}, ${peso.value}, ${altura.value} </p>`)
+    }
+
+
+    form.addEventListener('submit', recebeEventoForm );
+
+
+
+
+
+
+}
+escopo();
---
+++ Todos os dias/dia 12/revisão/aula.js
@@ -0,0 +1,12 @@
+
+// VARIÁVEIS camelCase 
+let nome = 'Silas';
+let altura = 170;
+pessoa = {
+    nome,
+    altura
+}
+console.log(pessoa);
+
+array = [1 , 2, 3];
+console.log(array)
