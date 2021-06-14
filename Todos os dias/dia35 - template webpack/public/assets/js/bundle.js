1cc5dd82b131a08943811ed27fe14948832aea16
silasbispo01 <silasbispo01@gmail.com>
2021-06-14 15:21:15 -0300

    webpack template

---
+++ Todos os dias/dia35 - template webpack/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('hello world');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
