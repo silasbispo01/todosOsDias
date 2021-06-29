41f2173f316a533c488a994c41e4da72ee05bf79
silasbispo01 <silasbispo01@gmail.com>
2021-06-29 16:40:12 -0300

    docs(paths): some of paths

---
+++ Todos os dias/dia38/paths/__paths.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+// Voltou 1 pasta
+console.log(path.resolve(__dirname, '..'))
+
+// Caminho da pasta atual
+console.log(__dirname)
+
+// Caminho do arquivo atual
+console.log(__filename)
